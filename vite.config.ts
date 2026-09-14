168259f1d46c2af0be39710cf41101bc0d826061
lina_artworks <kaolin.reb@gmail.com>
2026-09-14 23:42:33 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/sweet-memory-game/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
